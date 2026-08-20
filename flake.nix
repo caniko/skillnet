8175602eced2f5dbe84747e3a4384a662da8658f
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-20 13:36:06 +0200

    chore: bump harbor pins to new rev

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "skillnet AI skill mirror manager";
 
   inputs = {
-    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=9bfa8bdb0ecb22d7bc11448665f7fbaebae7a759";
+    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
 
     nixpkgs.follows = "rs-harbor/nixpkgs";
     rust-overlay.follows = "rs-harbor/rust-overlay";
